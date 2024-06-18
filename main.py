d342cf8ac57769510f812db2d25148dc1955a666
Gaurav Kumar <gaurav.iitp.cs17@gmail.com>
2024-06-18 21:09:11 +0530

    add the wsgi type server and a request handler

--- main.py
+++ main.py
@@ -1,50 +1,20 @@
-import socket
-import threading
+import sys
 
-from reqres import Request
-from routers import Router
+from server import WSGIServer
 
-local_file_dir = ""
-HOST, PORT = "localhost", 4221
-
-def start_server_and_server_requests(config: dict|None = None):
-    """
-        server must perform - socket(), bind(), listen(), accept()
-        client must perform - socket(), connect()
-    
-    """
-    if config is None:
-        config = {}
-    print("Starting server...")
-    try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((HOST, PORT))
-        server_socket.listen(1)
-        
-        while True:
-            conn, addr = server_socket.accept() # accept the connection
-            threading.Thread(target=handle_request, args=(conn, addr, config)).start() # handle the request
-    except KeyboardInterrupt:
-        print("\nClosing server...")
-    except Exception as e:
-        print(f"\nException: {e}")
-        print("\nClosing server...")
-
-def handle_request(conn, addr, config: dict):
-    data = conn.recv(1024)
-    print(f"Connected by {addr}")
-    if data == b"":
-        conn.close()
-        return
-    router = Router()
-    request = Request(data, config)
-    print(f"[request]: {str(request.__dict__)}")
-    response = router.route(request)
-    print(f"[response]: {response}")
-    conn.sendall(response)
-    conn.close()
-    return response
+def get_server(module, app):
+    app = getattr(__import__(module), app)
+    server = WSGIServer({
+        "host": "localhost", 
+        "port": 4221, 
+        "request_backlog": 10
+    })
+    server.set_app(app)
+    return server
 
 if __name__ == "__main__":
-    start_server_and_server_requests()
+    if len(sys.argv) < 2:
+        sys.exit("need to provide wsgi app object e.g. 'main:app'")
+    module, app = sys.argv[1].split(":")
+    server = get_server(module, app)
+    server.serve_forever()
---
+++ server.py
@@ -0,0 +1,109 @@
+import datetime
+import io
+import socket
+    
+class WSGIServer:
+    
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    
+    def __init__(self, init_config: dict):
+        self.init_config = init_config
+        self._set_socket()
+        self._set_server_config()
+    
+    def _set_socket(self):        
+        addr_tuple = (
+            self.init_config.get("host", "localhost"), 
+            int(self.init_config.get("port", "4221"))
+        )
+        server_socket = socket.socket(self.address_family, self.socket_type)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(addr_tuple)
+        server_socket.listen(self.init_config.get("request_backlog", 1))
+        self.server_socket = server_socket  
+    
+    def _set_server_config(self):
+        host, port = self.server_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.server_host = host
+        self.server_config = {
+            "server_name": self.server_name,
+            "server_port": self.server_port,
+            "server_host": host,
+            "app": self.init_config.get("app"),
+            "server_version": "1.0",
+            "server_software": "WSGIServer 0.2",
+        }
+    
+    def set_app(self, app):
+        self.app = app
+    
+    def serve_forever(self):
+        print(f"WSGIServer: Serving HTTP on {self.server_host}:{self.server_port}")
+        while True:
+            conn, addr = self.server_socket.accept()
+            request_handler = RequestHandler(self.server_config, conn)
+            import threading
+            thread = threading.Thread(
+                target=request_handler.handle_request,
+                args=(conn, addr)
+            ).start()
+        
+
+class RequestHandler:
+    
+    def __init__(self, server_config, conn):
+        self.server_config = server_config
+        self.conn = conn
+        self.set_environ()
+    
+    def handle_request(self):
+        self.parse_request()
+        result = self.server_config.get("app")(self.environ, self.start_response)
+        self.finish_response(result)
+
+    def parse_request(self):
+        self.request_data = self.conn.recv(1024).decode("utf-8")
+        request_line = self.request_data.splitlines()[0].rstrip("\r\n")
+        (self.request_method, self.path, self.request_version) = request_line.split()
+        print(f"Request Method: {self.request_method} Path: {self.path} Request Data: {self.request_data}")
+    
+    def set_environ(self):
+        self.environ = {
+            "wsgi.version": (1, 0),
+            "wsgi.url_scheme": "http",
+            "wsgi.input": io.StringIO(self.request_data),
+            "wsgi.errors": "wsgi.errors",
+            "wsgi.multithread": True,
+            "wsgi.multiprocess": False,
+            "wsgi.run_once": False,
+            "REQUEST_METHOD": self.request_method,
+            "PATH_INFO": self.path,
+            "SERVER_NAME": self.server_config.get("server_name"),
+            "SERVER_PORT": str(self.server_config.get("server_port")),
+        }
+    
+    def start_response(self, status, response_headers):
+        server_headers = [
+            ("Date", datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT")),
+            ("Server", self.server_config.get("server_software")),
+        ]
+        self.headers_set = [status, response_headers + server_headers]
+        return self.finish_response
+    
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response = f"HTTP/1.1 {status}\r\n"
+            for header in response_headers:
+                response += f"{header[0]}: {header[1]}\r\n"
+            response += "\r\n"
+            for data in result:
+                response += data.decode("utf-8")
+            self.response = response
+            print(f"Response: {response}")
+            self.conn.sendall(response.encode())
+        finally:
+            self.conn.close()
